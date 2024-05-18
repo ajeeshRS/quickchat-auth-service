ad7562ea52c649cd2b56b96092924b66556cb573
ajeeshRS <unnikuttz300@gmail.com>
2024-05-18 17:33:23 +0530

    db config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import express from "express";
 import routes from "./routes/authRoutes"
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { connectDb } from "./config/connection"
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
 
 app.use(cors());
 app.use(express.json())
@@ -14,6 +15,9 @@ app.use(express.json())
 app.use("/api/v1/auth", routes)
 
 
+
 app.listen(PORT, () => {
+  connectDb()
   console.log(`Connected to auth service on port : ${PORT} `);
 });
+
