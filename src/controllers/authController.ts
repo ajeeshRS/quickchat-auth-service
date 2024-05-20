6b90e3150cfba7b31a316068436fd9d14c4ea68d
ajeeshRS <unnikuttz300@gmail.com>
2024-05-20 20:42:25 +0530

    Added login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt"
 import { user } from "../models/userModel"
 import axios from "axios"
+import jwt from "jsonwebtoken"
 
 export const signUp = async (req: Request, res: Response) => {
     try {
@@ -56,6 +57,42 @@ export const signUp = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
 
+    try {
+        const jwtSecret: string = process.env.JWT_SECRET as string
+
+        const { email, password, googleId } = req.body
+        if (!email || (!password && !googleId)) {
+            return res.status(400).json("All fields are mandatory")
+        }
+
+        const existingUser = await user.findOne({ email: email })
+
+        if (!existingUser) {
+            return res.status(404).json("User not found")
+        }
+
+        let match;
+        if (existingUser.password) {
+            match = await bcrypt.compare(password, existingUser.password)
+        }
+
+        if (existingUser && (match || googleId === existingUser.googleId)) {
+
+            const token = jwt.sign({
+                user: {
+                    email: existingUser.email
+                }
+            }, jwtSecret, { expiresIn: '3d' })
+
+            res.status(200).json({ message: "Login success", token: token })
+        } else {
+            return res.status(401).json("Invalid credentials")
+        }
+
+    } catch (err) {
+        console.error("Error in login", err)
+        res.status(500).json("Couldn't login")
+    }
 }
 
 
