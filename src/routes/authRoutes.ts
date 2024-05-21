40b868570aba58a458bc9d8efd9d51f0c25beef3
Ajeesh <ajeeshrs79@gmail.com>
2024-05-21 20:53:47 +0530

    Added rate limiting

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,14 @@
 import express from "express"
 import { login, resetPassword, sendOtp, signUp, verifyOtp } from "../controllers/authController"
+import rateLimit from "express-rate-limit"
 
 const router = express.Router()
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+})
 
 router.post("/signup", signUp)
 
@@ -10,7 +16,7 @@ router.post("/login", login)
 
 router.post("/send-otp", sendOtp)
 
-router.post("/verify-otp", verifyOtp)
+router.post("/verify-otp", limiter, verifyOtp)
 
 router.post("/reset-password", resetPassword)
 
